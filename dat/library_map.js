e9e5e57979c1e06cb00ca925f6852a3a3c8106f8
Shaunak Kulkarni <155657463+k-shaunak@users.noreply.github.com>
2025-12-16 19:23:04 +0530

    Add LIBRARY_MAP for reference mapping modularisation

---
+++ dat/library_map.js
@@ -0,0 +1,93 @@
+/**
+ * THE ARCHIVIST - LIBRARY MAP
+ * External research repositories.
+ */
+
+window.LIBRARY_MAP = {
+    "General Reference": [
+        { name: "Google Scholar", url: "https://scholar.google.com/scholar?q=" },
+        { name: "JSTOR", url: "https://www.jstor.org/action/doBasicSearch?Query=" },
+        { name: "WorldCat", url: "https://www.worldcat.org/search?q=" },
+        { name: "Internet Archive", url: "https://archive.org/search.php?query=" },
+        { name: "Encyclopaedia Britannica", url: "https://www.britannica.com/search?query=" },
+        { name: "Oxford Reference", url: "https://www.oxfordreference.com/search?q=" },
+        { name: "Cambridge Dictionary of Philosophy", url: "https://www.cambridge.org/core/search?q=" }
+    ],
+    "Philosophy & Theory": [
+        { name: "Stanford Encyclopedia (SEP)", url: "https://plato.stanford.edu/search/searcher.py?query=" },
+        { name: "Internet Encyclopedia (IEP)", url: "https://iep.utm.edu/?s=" },
+        { name: "PhilPapers", url: "https://philpapers.org/s/" },
+        { name: "Notre Dame Philosophical Reviews", url: "https://ndpr.nd.edu/search/?q=" },
+        { name: "Oxford Handbooks Online (Philosophy)", url: "https://www.oxfordhandbooks.com/search?q=" },
+        { name: "Routledge Encyclopedia of Philosophy", url: "https://www.rep.routledge.com/search?query=" },
+        { name: "Aeon", url: "https://aeon.co/search?query=" }
+    ],
+    "Science & Tech": [
+        { name: "ArXiv.org", url: "https://arxiv.org/search/?query=" },
+        { name: "Nature.com", url: "https://www.nature.com/search?q=" },
+        { name: "PubMed", url: "https://pubmed.ncbi.nlm.nih.gov/?term=" },
+        { name: "IEEE Xplore", url: "https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=" },
+        { name: "ACM Digital Library", url: "https://dl.acm.org/action/doSearch?AllField=" },
+        { name: "Scientific American", url: "https://www.scientificamerican.com/search/?q=" },
+        { name: "New Scientist", url: "https://www.newscientist.com/search/?q=" },
+        { name: "Quanta Magazine", url: "https://www.quantamagazine.org/?s=" },
+        { name: "MIT Technology Review", url: "https://www.technologyreview.com/search/?s=" },
+        { name: "Physics Today", url: "https://physicstoday.scitation.org/action/doSearch?AllField=" },
+        { name: "American Scientist", url: "https://www.americanscientist.org/search?query=" }
+    ],
+    "Arts & Culture": [
+        { name: "Google Arts & Culture", url: "https://artsandculture.google.com/search?q=" },
+        { name: "The Art Story", url: "https://www.theartstory.org/search-results.htm?q=" },
+        { name: "MoMA Collection", url: "https://www.moma.org/search/?query=" },
+        { name: "Tate Collection", url: "https://www.tate.org.uk/search?q=" },
+        { name: "Smarthistory", url: "https://smarthistory.org/?s=" },
+        { name: "BFI Screenonline", url: "https://www.screenonline.org.uk/search.php?q=" },
+        { name: "Poetry Foundation", url: "https://www.poetryfoundation.org/search?query=" },
+        { name: "Art Institute of Chicago – Archives", url: "https://www.artic.edu/research/archives/search?query=" },
+        { name: "Ryerson & Burnham Archives (AIC)", url: "https://www.artic.edu/research/archives/collections?query=" },
+        { name: "Chicago Film Archives", url: "https://www.chicagofilmarchives.org/search?query=" },
+        { name: "Newberry Library Digital Collections", url: "https://collections.newberry.org/CS.aspx?VP3=SearchResult&VBID=" }
+    ],
+    "Social Sciences": [
+        { name: "SSRN", url: "https://papers.ssrn.com/sol3/results.cfm?txtKey_Words=" },
+        { name: "APA PsycNet", url: "https://psycnet.apa.org/search/results?term=" },
+        { name: "SAGE Journals", url: "https://journals.sagepub.com/action/doSearch?AllField=" },
+        { name: "SocArXiv", url: "https://osf.io/preprints/socarxiv/search?q=" },
+        { name: "The Sociological Review", url: "https://journals.sagepub.com/search?SeriesKey=tsr" },
+        { name: "LSE Review of Books", url: "https://blogs.lse.ac.uk/lsereviewofbooks/?s=" },
+        { name: "Brookings", url: "https://www.brookings.edu/search/?s=" }
+    ],
+    "Economics & Finance": [
+        { name: "NBER", url: "https://www.nber.org/search?q=" },
+        { name: "The Economist", url: "https://www.economist.com/search?q=" },
+        { name: "Investopedia", url: "https://www.investopedia.com/search?q=" },
+        { name: "FRED", url: "https://fred.stlouisfed.org/searchresults?st=" },
+        { name: "IMF Data", url: "https://www.imf.org/en/Search#q=" },
+        { name: "Financial Times", url: "https://www.ft.com/search?q=" },
+        { name: "VoxEU / CEPR", url: "https://cepr.org/search?query=" },
+        { name: "Project Syndicate", url: "https://www.project-syndicate.org/search?query=" },
+        { name: "Econlib", url: "https://www.econlib.org/?s=" },
+        { name: "Marginal Revolution", url: "https://marginalrevolution.com/?s=" },
+        { name: "RePEc (Research Papers in Economics)", url: "https://ideas.repec.org/search.html?q=" },
+        { name: "ECB Working Papers", url: "https://www.ecb.europa.eu/pub/research/working-papers/html/index.en.html?search=" },
+        { name: "ECB Economic Bulletins", url: "https://www.ecb.europa.eu/pub/economic-bulletin/html/index.en.html?search=" },
+        { name: "Harvard Growth Lab", url: "https://growthlab.hks.harvard.edu/search?keywords=" }
+    ],
+    "Law & Policy": [
+        { name: "Legal Info Institute", url: "https://www.law.cornell.edu/search/lii?query=" },
+        { name: "Congressional Research", url: "https://crsreports.congress.gov/search/#/?termsToSearch=" },
+        { name: "Justia", url: "https://law.justia.com/search?q=" },
+        { name: "HeinOnline", url: "https://heinonline.org/HOL/Search?query=" },
+        { name: "Yale Law Journal", url: "https://www.yalelawjournal.org/search?q=" },
+        { name: "Harvard Law Review", url: "https://harvardlawreview.org/?s=" },
+        { name: "Verfassungsblog", url: "https://verfassungsblog.de/?s=" }
+    ],
+    "Mathematics": [
+        { name: "Wolfram MathWorld", url: "https://mathworld.wolfram.com/search/?query=" },
+        { name: "OEIS", url: "https://oeis.org/search?q=" },
+        { name: "Math StackExchange", url: "https://math.stackexchange.com/search?q=" },
+        { name: "Plus Magazine", url: "https://plus.maths.org/content/search?query=" },
+        { name: "Quanta (Math)", url: "https://www.quantamagazine.org/mathematics/?s=" },
+        { name: "AMS Feature Columns", url: "https://www.ams.org/search?query=" }
+    ]
+};
